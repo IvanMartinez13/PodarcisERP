09d4d90e401df24dd6a2f21215e933c8e60067bc
Ivan Martinez <informatica@podarcisspain.onmicrosoft.com>
2022-02-17 13:19:27 +0100

    plantilla and lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,72 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Styles -->
+    <link href="{{ url('/') }}/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ url('/') }}/fontawesome/css/all.min.css" rel="stylesheet">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+    <link href="{{ url('/') }}/css/animate.css" rel="stylesheet">
+    <link href="{{ url('/') }}/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        {{-- SIDEBAR --}}
+        @include('shared.sidebar')
+
+        <div id="page-wrapper" class="gray-bg">
+            {{-- TOPNAV --}}
+            @include('shared.topnav')
+
+            {{-- CONTENT --}}
+            <div class="wrapper wrapper-content animated fadeInRight">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center m-t-lg">
+                            @yield('content')
+                        </div>
+                    </div>
                 </div>
-            </header>
+            </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            {{-- FOOTER --}}
+            @include('shared.footer')
+
+            <form id="logout_form" action="{{ route('logout') }}" method="POST">
+                @csrf
+            </form>
+
         </div>
-    </body>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="{{ url('/') }}/js/jquery-3.1.1.min.js"></script>
+    <script src="{{ url('/') }}/js/popper.min.js"></script>
+    <script src="{{ url('/') }}/js/bootstrap.min.js"></script>
+    <script src="{{ url('/') }}/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="{{ url('/') }}/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{ url('/') }}/js/inspinia.js"></script>
+    <script src="{{ url('/') }}/js/plugins/pace/pace.min.js"></script>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+</body>
+
 </html>
---
+++ resources/views/shared/sidebar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <span class="block m-t-xs font-bold">{{ auth()->user()->name }}</span>
+                        <span class="text-muted text-xs block">menu <b class="caret"></b></span>
+                    </a>
+                    <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                        <li><a class="dropdown-item" type="button" onclick="$('#logout_form').submit()">Logout</a></li>
+                    </ul>
+                </div>
+                <div class="logo-element">
+                    ODS
+                </div>
+            </li>
+
+            <li class="{{ request()->is('dashboard') ? 'active' : '' }}">
+                <a href="{{ route('dashboard') }}"><i class="fa-solid fa-gauge-high"></i> <span
+                        class="nav-label">{{ __('modules.dashboard') }}</span></a>
+            </li>
+
+            @can('super-admin')
+                <li class="{{ request()->is('customers/') ? 'active' : '' }}">
+                    <a href="{{ route('dashboard') }}"><i class="fa-solid fa-user-tie"></i> <span
+                            class="nav-label">{{ __('modules.customers') }}</span></a>
+                </li>
+            @endcan
+        </ul>
+
+    </div>
+</nav>
