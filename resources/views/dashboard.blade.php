9436118cad649982f5f9198e0a61fd9f6e55c978
Ivan Martinez <informatica@podarcisspain.onmicrosoft.com>
2022-03-10 13:59:48 +0100

    distrubution sp_admin dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">
-        <h1>
-            EL DASHBOARD NO SE ENCUENTRA DISPONIBLE TODAVIA.
-        </h1>
-        <p>Estamos trabajando en ello ;)</p>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Clientes</h5>
+                </div>
+                <div class="ibox-content">
+                    <h5>Usuarios</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Módulos</h5>
+                </div>
+                <div class="ibox-content">
+                    <h5>Usuarios</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Espacio Ocupado</h5>
+                </div>
+                <div class="ibox-content">
+                    <h5>Usuarios</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Mensajes</h5>
+                </div>
+                <div class="ibox-content">
+                    <h5>Usuarios</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>In box</h5>
+                </div>
+
+                <div class="ibox-content">
+                    <h5>Tabla con incidencias de los usuarios</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Blog</h5>
+                </div>
+
+                <div class="ibox-content">
+                    <h5>Entrads del blog</h5>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
